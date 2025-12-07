69f7829ab908e7e473e29b9b80db68d4f0884921
Anderson Lopes <developesanderson@gmail.com>
2025-12-07 00:58:56 -0300

    Update security headers and add permissive CSP for production in Netlify

--- next.config.ts
+++ next.config.ts
@@ -57,6 +57,24 @@ const baseConfig = {
             value:
               'accelerometer=(), camera=(), geolocation=(), microphone=(), payment=(), usb=()',
           },
+          // CSP permissivo para produção (sem nonce pois middleware está desabilitado no Netlify)
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com https://apis.google.com https://*.googletagmanager.com https://www.gstatic.com https://us.i.posthog.com",
+              "script-src-elem 'self' 'unsafe-inline' https://accounts.google.com https://apis.google.com https://*.googletagmanager.com https://www.gstatic.com https://us.i.posthog.com",
+              "worker-src 'self' blob:",
+              "connect-src 'self' https://*.googleapis.com https://apis.google.com https://*.firebaseio.com https://*.cloudfunctions.net wss://*.firebaseio.com https://*.ingest.us.sentry.io https://*.ingest.sentry.io https://securetoken.googleapis.com https://identitytoolkit.googleapis.com https://*.google.com https://www.googleapis.com https://*.r2.cloudflarestorage.com https://us.i.posthog.com https://*.posthog.com",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://www.gstatic.com",
+              "font-src 'self' data: https://fonts.gstatic.com https://www.gstatic.com",
+              "img-src 'self' data: https: blob:",
+              "media-src 'self' https://*.r2.cloudflarestorage.com blob: data:",
+              "frame-src 'self' https://accounts.google.com https://*.firebaseapp.com",
+              "form-action 'self' https://accounts.google.com",
+              "frame-ancestors 'self'",
+            ].join('; '),
+          },
           // HSTS apenas em produção com HTTPS
           ...(process.env.NODE_ENV === 'production'
             ? [
